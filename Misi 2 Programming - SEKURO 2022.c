2bbbd494c57218fcc5048957406a42b2b0849cfa
Brianaldo <79987913+Brianaldo@users.noreply.github.com>
2022-02-11 09:26:06 +0700

    First Commit

---
+++ Misi 2 Programming - SEKURO 2022.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+float fPlus(float a, float b) {
+    /* Function tambah */
+    /* Mengembalikan value a + b */
+}
+
+void pPlus(float *a, float b) {
+    /* Procedure tambah */
+    /* I.S. a dan b terdefinisi dan tidak sembarang */
+    /* F.S. a menjadi a + b */
+}
+
+float fMin(float a, float b) {
+    /* Function kurang */
+    /* Mengembalikan value a + b */
+}
+
+void pMin(float *a, float b) {
+    /* Procedure kurang */
+    /* I.S. a dan b terdefinisi dan tidak sembarang */
+    /* F.S. a menjadi a - b */
+}
+
+float fTimes(float a, float b) {
+    /* Function kali */
+    /* Mengembalikan value a * b */
+}
+
+void pTimes(float *a, float b) {
+    /* Procedure kali */
+    /* I.S. a dan b terdefinisi dan tidak sembarang */
+    /* F.S. a menjadi a * b */
+}
+
+float fDevide(float a, float b) {
+    /* Function bagi */
+    /* Mengembalikan value a / b */
+}
+
+void pDevide(float *a, float b) {
+    /* Procedure bagi */
+    /* I.S. a dan b terdefinisi dan tidak sembarang */
+    /* F.S. a menjadi a * b */
+}
+
+float fPower(float a, int b) {
+    /* Function pangkat */
+    /* Mengembalikan value a ^ b */
+}
+
+void pPower(float *a, int b) {
+    /* Procedure pangkat */
+    /* I.S. a dan b terdefinisi dan tidak sembarang */
+    /* F.S. a menjadi a ^ b */
+}
+
+int main() {
+    /* Buatlah kalkulator integral menggunakan konsep Integral Riemann */
+    /* Notes: Penggunaan operator aritmatika harus menggunakan fungsi dan 
+              prosedur yang telah didefinisikan dan diimplementasi sebelumnya */
+    /* Input dan output dibebaskan kepada Cakru URO 14 */
+    /* Batasan: Derajat dari koefisien adalah whole number {0, 1, 2, ...} */
+    return 0;
+}
